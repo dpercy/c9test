e1ce451fb844c966445d1489b8a9aaaf77dfa6fd
Jason Zucchetto <jason@Jason-Zuchetto-MacBook-Pro-2.local>
2015-08-10 15:18:41 -0700

    Changed to ports 8080, 8081, 8082

--- Dockerfile
+++ Dockerfile
@@ -8,9 +8,9 @@ COPY 	./assets /assets
 RUN		mkdir /mongodb-3.0.4
 RUN		tar -xvf /assets/mongodb-linux-x86_64-ubuntu1404-3.0.4.tgz -C /mongodb-3.0.4
 
-EXPOSE  27017
-EXPOSE  27018
-EXPOSE  27019
+EXPOSE  8080
+EXPOSE  8081
+EXPOSE  8082
 
 # Run the mongo client, this is required to keep the container running
 CMD ["sh", "/assets/replica_set.sh"]
--- assets/replica_set.sh
+++ assets/replica_set.sh
@@ -8,26 +8,26 @@ rm -rf /data/rs1 /data/rs2 /data/rs3 /tmp/replset
 
 mkdir -p /data/rs1 /data/rs2 /data/rs3 /tmp/replset
 
-$MONGOPATH/mongod --replSet rs0 --logpath /tmp/replset/1.log --dbpath /data/rs1 --port 27017 --oplogSize 2 --fork --nojournal --smallfiles
-$MONGOPATH/mongod --replSet rs0 --logpath /tmp/replset/2.log --dbpath /data/rs2 --port 27018 --oplogSize 2 --fork --nojournal --smallfiles
-$MONGOPATH/mongod --replSet rs0 --logpath /tmp/replset/3.log --dbpath /data/rs3 --port 27019 --oplogSize 2 --fork --nojournal --smallfiles
+$MONGOPATH/mongod --replSet rs0 --logpath /tmp/replset/1.log --dbpath /data/rs1 --port 8080 --oplogSize 2 --fork --nojournal --smallfiles
+$MONGOPATH/mongod --replSet rs0 --logpath /tmp/replset/2.log --dbpath /data/rs2 --port 8081 --oplogSize 2 --fork --nojournal --smallfiles
+$MONGOPATH/mongod --replSet rs0 --logpath /tmp/replset/3.log --dbpath /data/rs3 --port 8082 --oplogSize 2 --fork --nojournal --smallfiles
 
 sleep 3 
 
 HOSTNAME="$(hostname)"
 
 sleep 30
-$MONGOPATH/mongo --eval 'printjson(rs.initiate({ "_id": "rs0", "members" : [{ "_id" : 0, "host" : "127.0.0.1:27017" },{ "_id" : 1, "host" : "127.0.0.1:27018" },{ "_id" : 2, "host" : "127.0.0.1:27019" } ]}))'
+$MONGOPATH/mongo --port 8080 --eval 'printjson(rs.initiate({ "_id": "rs0", "members" : [{ "_id" : 0, "host" : "127.0.0.1:8080" },{ "_id" : 1, "host" : "127.0.0.1:8081" },{ "_id" : 2, "host" : "127.0.0.1:8082" } ]}))'
 sleep 45
 
-$MONGOPATH/mongo --eval "for (i = 0; i < 1000; i++ ) db.col.insert( { val : 'a' })"
+$MONGOPATH/mongo --port 8080 --eval "for (i = 0; i < 1000; i++ ) db.col.insert( { val : 'a' })"
 
 sleep 10
 
-$MONGOPATH/mongo --port 27019 --eval "db.adminCommand( { shutdown : 1 })"
+$MONGOPATH/mongo --port 8082 --eval "db.adminCommand( { shutdown : 1 })"
 
 sleep 10
 
-$MONGOPATH/mongo --eval "for (i = 0; i < 200000; i++ ) db.col.insert( { val : 'a' })"
+$MONGOPATH/mongo --port 8080 --eval "for (i = 0; i < 200000; i++ ) db.col.insert( { val : 'a' })"
 
-$MONGOPATH/mongod --replSet rs0 --logpath /tmp/replset/3.log --dbpath /data/rs3 --port 27019 --oplogSize 2 --fork --nojournal --smallfiles
+$MONGOPATH/mongod --replSet rs0 --logpath /tmp/replset/3.log --dbpath /data/rs3 --port 8082 --oplogSize 2 --fork --nojournal --smallfiles
